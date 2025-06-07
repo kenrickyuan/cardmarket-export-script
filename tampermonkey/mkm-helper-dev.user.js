84029ff24bbfba6fbb257bb968861cc8fbc11949
Kenrick <kenrick.yuan@ingka.ikea.com>
2025-06-07 12:32:42 +0200

    feat: add Tampermonkey userscript configurations
    - Add production userscript header template for Magic: The Gathering
    - Add development userscript that loads from localhost:3000
    - Focus on CardMarket Magic pages only, removing Flesh and Blood support

---
+++ tampermonkey/mkm-helper-dev.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         MKM Helper (DEV)
+// @namespace    https://github.com/your-username/mkm-helper
+// @version      0.0.1-dev
+// @description  Development version - loads from localhost
+// @author       Your Name
+// @match        https://www.cardmarket.com/*/Magic/Products/*/*
+// @match        https://www.cardmarket.com/*/Magic/Cards/*
+// @match        https://www.cardmarket.com/*/Magic/Wants/*
+// @match        https://www.cardmarket.com/*/Magic/Orders/*
+// @match        https://www.cardmarket.com/*/Magic/ShoppingCart
+// @match        https://www.cardmarket.com/*/Magic/Users/*/Offers/Singles*
+// @match        https://www.cardmarket.com/*/Magic/Orders/Search/Results*
+// @require      http://localhost:3000/script.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cardmarket.com
+// @grant        none
+// ==/UserScript==
+
+// Development script - all code loaded from @require directive above
+console.log('🚀 MKM Helper DEV loader initialized');
